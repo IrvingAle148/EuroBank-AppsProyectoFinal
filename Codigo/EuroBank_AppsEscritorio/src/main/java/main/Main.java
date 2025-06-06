dd289eb83b158fa5f9ced3a64470f6bf0654cf7d
IrvingAle148 <irvingaseguinluna@gmail.com>
2025-06-06 00:49:21 -0600

    Update: Clases FXML

--- Codigo/EuroBank_AppsEscritorio/src/main/java/main/Main.java
+++ Codigo/EuroBank_AppsEscritorio/src/main/java/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginMainView.fxml"));
 
         Scene scene = new Scene(root, 800, 600);
         scene.getStylesheets().add(getClass().getResource("/styles/application.css").toExternalForm());
